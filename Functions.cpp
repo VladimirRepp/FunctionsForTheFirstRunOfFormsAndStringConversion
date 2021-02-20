7008031c2b4de2a106bde0c89d125e0733d36076
FPS.FirePlanetStudio <57412538+VladimirRepp@users.noreply.github.com>
2021-02-20 11:02:48 +0700

    Add files via upload

---
+++ Functions.cpp
@@ -0,0 +1,41 @@
+/*======================= Function for launching the form =======================*/
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	//ProjectName - name of your project
+	//NameForm - name of the form to run
+	ProjectName::NameForm form;
+	Application::Run(% form);
+}
+
+/*======================= Converting System::String ^ to std::string and vice versa =======================*/
+#include <string>
+using namespace System;
+
+//Function prototypes (for the header file -> .h)
+std::string ConvertToString(System::String^ s);		//ñonverting System::string ^ to std::string
+String^ ConvertToString(std::string& os);			//ñonverting std::string to System::string ^
+
+//Implementing functions (for the execution file -> .cpp)
+//Converting System::string ^ â std::string
+std::string ConvertToString(System::String^ s) {
+	using namespace Runtime::InteropServices;
+	const char* chars =
+		(const char*)(Marshal::StringToHGlobalAnsi(s)).ToPointer();
+	std::string	os = chars;
+	Marshal::FreeHGlobal(IntPtr((void*)chars));
+
+	return os;
+}
+
+//Converting std::string â System::string ^ 
+System::String^ ConvertToString(std::string& os) {
+	System::String^ s = gcnew System::String(os.c_str());
+
+	return s;
+}
---
+++ README.txt
@@ -0,0 +1,15 @@
+# FunctionsForTheFirstRunOfFormsAndStringConversion
+Полезные функции для первого запуска форм и преобразования строк из управляемых в стандартные и наоборот 
+
+© FirePlanetStudio
+
+Language: C++ 
+Interface: Windows Forms 
+Version: 0.1
+
+В данном файле приведены примеры функций для:
+1. Запуска окна Windows Forms
+2. Преобразования управляемой строки в стандартную (System::String^ в std::string)
+3. Преобразования стандартной строки в управляемую (std::string в System::String^)
+
+Проект создан в обучающих целях.
